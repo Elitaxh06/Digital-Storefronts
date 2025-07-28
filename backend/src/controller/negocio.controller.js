06dbd5b6829af436ea201cac8204c0847210abe3
Elitaxh <vivaheredia2535@outlook.com>
2025-07-27 18:09:13 -0600

    .Env arreglado con rutas nuevas

--- backend/src/controller/negocio.controller.js
+++ backend/src/controller/negocio.controller.js
@@ -7,21 +7,18 @@ dotenv.config()
 const mensaje = 'Este endpoint devuelve '
 export const listarNegocios = async(req, res) => {
     try{
-        // console.log('URL:', process.env.URL_GET_NEGOCIOS);
-        // console.log('API KEY:', process.env.API_KEY);
-        // console.log('Authorization:', process.env.Authorization);
-        // console.log('Entro en el try del controller')
        const { data } = await axios.post(
             process.env.URL_GET_NEGOCIOS,
             {},
             {
                 headers: {
                     "Content-Type": "application/json",
-                    "apikey" : process.env.API_KEY,
-                    "Authorization" : `Bearer ${process.env.Authorization}`
+                    "apikey" : process.env.API_KEY_SERVICE_ROLE,
+                    "Authorization" : `Bearer ${process.env.AUTHORIZATION_SERVICE_ROLE}`
                 }
             }
        )
+       console.log('Respuesta de Supabase:', data);
 
        const result = data
 
@@ -63,13 +60,13 @@ export const insertNegocio = async(req, res) => {
     try{
         const { p_nombre,p_descripcion,p_email,p_telefono,p_direccion,p_red_social_1,p_red_social_2,p_img_url_1,p_img_url_2,p_img_url_3,p_id_admin,p_id_categoria,p_estado } = req.body
         const { data } = await axios.post(
-            process.env.URL_INSERT_NEGOCIOS,
+            process.env.URL_INSERT_NEGOCIO,
             { p_nombre,p_descripcion,p_email,p_telefono,p_direccion,p_red_social_1,p_red_social_2,p_img_url_1,p_img_url_2,p_img_url_3,p_id_admin,p_id_categoria,p_estado  },
             {
                 headers: {
                     "Content-Type": "application/json",
-                    "apikey" : process.env.API_KEY,
-                    "Authorization" : `Bearer ${process.env.Authorization}`
+                    "apikey" : process.env.API_KEY_SERVICE_ROLE,
+                    "Authorization" : `Bearer ${process.env.AUTHORIZATION_SERVICE_ROLE}`
                 }
             }
         )
--- frontend/src/view/Login/Register.tsx
+++ frontend/src/view/Login/Register.tsx
@@ -20,7 +20,7 @@ function Register(){
 
         const phoneRegex = /^[0-9]{8}$/ 
         if(!phoneRegex.test(phoneNumber)){
-            showInfoAlert("El número de teléfono debe tener exactamente 8 dígitos.", "info")   
+            showInfoAlert("El número de teléfono debe contener solo numeros y tiene que ser de 8 dígitos.", "info")   
             return
         }
         const { data, error } = await supabase.auth.signUp({
@@ -46,7 +46,7 @@ function Register(){
         setName("")
     }
     return(
-        <section className="mt-24 mb-22">
+        <section className="mt-24 mb-22 px-4">
             <h2 className="text-center font-bold text-3xl">Crear Cuenta</h2>
             {message && (
                 <div className="flex justify-center my-5">
@@ -56,7 +56,7 @@ function Register(){
 
             <form className="max-w-md mx-auto" onSubmit={handleSubmit}>
             <div className="relative z-0 w-full mb-5 group">
-                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder=" " name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" required />
+                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder=" " name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-base text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" required />
                 <label htmlFor="floating_email" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Correo Electronico</label>
             </div>
             <div className="relative z-0 w-full mb-5 group">
@@ -65,16 +65,13 @@ function Register(){
             </div>
             <div className="grid md:grid-cols-2 md:gap-6">
                 <div className="relative z-0 w-full mb-5 group">
-                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} name="floating_first_name" id="floating_first_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} name="floating_first_name" id="floating_first_name" className="block py-2.5 px-0 w-full text-base text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                     <label htmlFor="floating_first_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Nombre</label>
                 </div>
-            </div>
-            <div className="grid md:grid-cols-2 md:gap-6">
                 <div className="relative z-0 w-full mb-5 group">
-                    <input type="tel" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} placeholder=" "  name="floating_phone" id="floating_phone" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"  required />
+                    <input type="tel" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} placeholder=" "  name="floating_phone" id="floating_phone" className="block py-2.5 px-0 w-full text-base text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"  required />
                     <label htmlFor="floating_phone" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Teléfono</label>
                 </div>
-                
             </div>
             <div className="flex items-center justify-between mb-6">
                 <button type="submit" className="text-white cursor-pointer bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Crar Cuenta</button>
