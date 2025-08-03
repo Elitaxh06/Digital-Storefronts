7dbbe6f78caf15c82011f939388d8d335d1f4955
Elitaxh <vivaheredia2535@outlook.com>
2025-08-02 23:32:15 -0600

    Arreglo del .env

--- frontend/src/Lib/SupabaseClient.ts
+++ frontend/src/Lib/SupabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-    import.meta.env.VITE_SUPABAE_URL,
+    import.meta.env.VITE_SUPABASE_URL,
     import.meta.env.VITE_SUPABSE_ANON_KEY
 )
 export default supabase
